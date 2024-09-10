cf5d4b7ee4f79a8eb31f33e438cc871b81c00d5c
Jincheol Shin <114928709+justgotothedesk@users.noreply.github.com>
2024-09-10 21:02:35 +0900

    Create 나무자르기(14247).py

---
+++ BackJoon/나무자르기(14247).py
@@ -0,0 +1,11 @@
+n = int(input())
+
+trees = list(zip(map(int, input().split()), map(int, input().split())))
+
+trees.sort(key=lambda x: (x[1], x[0]))
+
+answer = 0
+for i in range(n):
+    answer += (trees[i][0] + (i * trees[i][1]))
+  
+print(answer)

e18be3e473bc46290f3d5ec20f958fba7594e8f2
Jincheol Shin <114928709+justgotothedesk@users.noreply.github.com>
2024-09-24 20:07:59 +0900

    Create 신입 사원(1946).py

---
+++ BackJoon/신입 사원(1946).py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+T = int(input())
+
+for tc in range(T):
+    N = int(input())
+    freshman = [list(map(int, input().split())) + [i + 1] for i in range(N)]
+
+    freshman.sort(key=lambda x: x[0])
+
+    rank = freshman[0][1]
+    cnt = 1
+
+    for i in range(1, N):
+        if rank > freshman[i][1]:
+            rank = freshman[i][1]
+            cnt += 1
+
+    print(cnt)

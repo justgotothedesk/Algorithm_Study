2c05d91230a5a4ad81457c42b2f8a2b2fbd93445
Jincheol Shin <114928709+justgotothedesk@users.noreply.github.com>
2024-11-13 20:10:55 +0900

    Update 줄 서는 방법.py

--- Programmers/줄 서는 방법.py
+++ Programmers/줄 서는 방법.py
@@ -1,22 +1,41 @@
-def dfs(temp, data, result):
-    if not temp:
-        result.append(data)
+# 시간 초과 방법
+# def dfs(temp, data, result):
+#     if not temp:
+#         result.append(data)
         
-        return
+#         return
     
-    for i in range(len(temp)):
-        dfs(temp[:i]+temp[i+1:], data+[temp[i]], result)
+#     for i in range(len(temp)):
+#         dfs(temp[:i]+temp[i+1:], data+[temp[i]], result)
+
+# def solution(n, k):
+#     answer = []
+#     temp = []
+#     result = []
+    
+#     for i in range(1, n+1):
+#         temp.append(i)
+    
+#     dfs(temp, [], result)
+        
+#     answer = result[k-1]
+    
+#     return answer
+
+import math
 
 def solution(n, k):
     answer = []
-    temp = []
-    result = []
+    number = []
     
     for i in range(1, n+1):
-        temp.append(i)
+        number.append(i)
     
-    dfs(temp, [], result)
-        
-    answer = result[k-1]
-    
+    while number:
+        perm = math.factorial(n-1)
+        idx = (k-1)//perm
+        answer.append(number.pop(idx))
+        k %= perm
+        n -= 1
+
     return answer

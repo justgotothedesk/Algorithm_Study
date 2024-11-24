7955ef39d7825d6bb8e3ea74a5a38ed85e610fab
Jincheol Shin <114928709+justgotothedesk@users.noreply.github.com>
2024-11-24 16:50:46 +0900

    Create 수 정렬하기(2750).py

---
+++ BackJoon/수 정렬하기(2750).py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline()
+
+n = int(input())  
+members = []      
+
+for i in range(n):
+    age, name = input().split()
+    members.append((int(age), name, i))
+
+members.sort(key=lambda x: (x[0], x[2]))
+
+for member in members:
+    print(member[0], member[1]) 

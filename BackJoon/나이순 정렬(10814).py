e4e6464268c68a8f9b81b9a35ff8f74d86abf7eb
Jincheol Shin <114928709+justgotothedesk@users.noreply.github.com>
2024-11-25 19:35:29 +0900

    Create 나이순 정렬(10814).py

---
+++ BackJoon/나이순 정렬(10814).py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline()
+
+n = int(input())
+member = []
+
+for i in range(n):
+    age, name = map(str, input().split())
+    age = int(age)
+    member.append((age, name))
+
+member.sort(key = lambda x : x[0])
+
+for i in member:
+    print(i[0], i[1])

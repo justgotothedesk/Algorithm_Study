79823b32c72df3026f8a2fb44acf55746e94580c
Jincheol Shin <114928709+justgotothedesk@users.noreply.github.com>
2024-12-14 22:57:01 +0900

    Create 달팽이는 올라가고 싶다(2869).py

---
+++ BackJoon/달팽이는 올라가고 싶다(2869).py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+A, B, V = map(int, input().split())
+
+if V < A : 
+    print(1)
+else:
+    if (V-A) % (A-B) == 0 :
+        print((V-A) // (A-B) +1)
+    else :
+        print((V-A) // (A-B) +2)

39bdea7d1630f3db05364b0e76e5fe2446bb00fc
Jincheol Shin <114928709+justgotothedesk@users.noreply.github.com>
2024-12-15 18:47:24 +0900

    Create 영역 구하기(2583).py

---
+++ BackJoon/영역 구하기(2583).py
@@ -0,0 +1,47 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+x, y, n = map(int, input().split())
+
+graph = [[True]*y for _ in range(x)]
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+answer = []
+
+for _ in range(n):
+    x1, y1, x2, y2 = map(int, input().split())
+    
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            graph[j][i] = False
+
+def bfs(r, c):
+    q = deque()
+    q.append((r, c))
+    dist = 0
+    
+    while q:
+        a, b = q.popleft()
+        dist += 1
+        
+        for i in range(4):
+            nx = a+dx[i]
+            ny = b+dy[i]
+            
+            if 0 <= nx < x and 0 <= ny < y and graph[nx][ny]:
+                graph[nx][ny] = False
+                q.append((nx, ny))
+    
+    return dist
+
+for i in range(len(graph)):
+    for j in range(len(graph[i])):
+        if graph[i][j]:
+            graph[i][j] = False
+            value = bfs(i, j)
+            answer.append(value)
+
+answer.sort()
+print(len(answer))
+print(" ".join(map(str, answer)))

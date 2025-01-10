e8a6e6d22e2abea37907bf098a8d6da85da50ca5
Jincheol Shin <114928709+justgotothedesk@users.noreply.github.com>
2025-01-10 19:43:33 +0900

    Update 주차 요금 계산.py

--- Programmers/주차 요금 계산.py
+++ Programmers/주차 요금 계산.py
@@ -1,35 +1,56 @@
 # 2022 KAKAO BLIND RECRUITMENT
 # 차근차근 해결하면 그렇게 어렵지는 않은 문제
 # 딕셔너리 두 개를 활용한다.
-# 없는 차량이 입차를 한다면, init 딕셔너리에 추가하고 출차를 했다면 init에서 삭제하고 result에 추가한다.
+# 없는 차량이 입차를 한다면, parking 딕셔너리에 추가하고 출차를 했다면 parking에서 삭제하고 result에 추가한다.
 # result를 딕셔너리에서 순차적으로 남아있는 시간을 기준으로 요금을 계산한다.
 
 import math
 
 def convert(time):
     hh,mm=time.split(":")
+    
     return 60*int(hh)+int(mm)
 
-def solution(fees, records):
-    init = {}
-    result = {}
-    for i in records:
-        time, num, inout = i.split()
-        if num not in result:
-            result[num] = 0
-        if inout == 'IN':
-            init[num] = convert(time)
-        else:
-            result[num] += convert(time)-init[num]
-            del init[num]
-    for key,val in init.items():
-        result[key] += 23*60+59-val
+def calculate(fees, total_time):
+    dtime, dfee, ptime, pfee = fees[0], fees[1], fees[2], fees[3]
     
+    if total_time <= dtime:
+        return dfee
+    return dfee+math.ceil((total_time-dtime)/ptime)*pfee
+
+def solution(fees, records):
     answer = []
-    for key,val in sorted(result.items()):
-        if val <= fees[0]:
-            answer.append(fees[1])
+    temp = []
+    parking = {}
+    result = {}
+    
+    for record in records:
+        time, number, do = record.split(" ")
+        time = convert(time)
+        
+        if do == "IN":
+            parking[number] = time
+        elif do == "OUT":
+            total_time = time-parking[number]
+            del parking[number]
+            
+            if number not in result.keys():
+                result[number] = total_time
+            else:
+                result[number] += total_time
+            
+    for number in parking.keys():
+        if number not in result.keys():
+            result[number] = convert("23:59")-parking[number]
         else:
-            answer.append(fees[1]+math.ceil((val-fees[0])/fees[2])*fees[3])
+            result[number] += convert("23:59")-parking[number]
+            
+    for number in result.keys():
+        temp.append([number, calculate(fees, result[number])])
+    
+    temp.sort(key = lambda x:x[0])
+    
+    for _, value in temp:
+        answer.append(value)
         
     return answer

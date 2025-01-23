2d9a0d1955eeb4b48a52790bf014a0e9ef93eb25
Jincheol Shin <114928709+justgotothedesk@users.noreply.github.com>
2025-01-23 19:37:09 +0900

    Create 큐(10845).py

---
+++ BackJoon/큐(10845).py
@@ -0,0 +1,33 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+stack = []
+for i in range(N):
+  cmd = sys.stdin.readline().strip().split()
+  if cmd[0] == 'push':
+    stack.append(cmd[1])
+  elif cmd[0] == 'pop':
+    if len(stack) != 0:
+      print(stack.pop(0))
+    else:
+      print(-1)
+  elif cmd[0] == 'size':
+    print(len(stack))
+  elif cmd[0] == 'empty':
+    if len(stack) == 0:
+      print(1)
+    else:
+      print(0)
+  elif cmd[0] == 'front':
+    if len(stack) != 0:
+      print(stack[0])
+    else:
+      print(-1)
+  elif cmd[0] == 'back':
+    if len(stack) != 0:
+      print(stack[-1])
+    else:
+      print(-1)
